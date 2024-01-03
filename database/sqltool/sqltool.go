bd7a4232e933c7edbd9f7b52d083162d49397251
lovesnowex <lsktw22@gmail.com>
2024-01-03 16:28:04 +0800

    Add sql open to sqltool

--- database/sqltool/sqltool.go
+++ database/sqltool/sqltool.go
@@ -3,8 +3,13 @@ package sqltool
 import (
 	"database/sql"
 
+	"github.com/LoveSnowEx/gotool/database"
 	sq "github.com/Masterminds/squirrel"
 )
+
+func Open(conf *database.DatabaseConfig) (db *sql.DB, err error) {
+	return sql.Open(conf.Driver, conf.Dsn())
+}
 
 func PK(db *sql.DB, table string) (pk string, err error) {
 	sql, args, err := sq.
