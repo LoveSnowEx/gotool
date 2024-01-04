5f714c9e3870ecfabc92c5d480e52ea8b35d4e71
lovesnowex <lsktw22@gmail.com>
2024-01-04 11:08:09 +0800

    Refactor gormtool.Open

--- database/gormtool/gormtool.go
+++ database/gormtool/gormtool.go
@@ -9,27 +9,27 @@ import (
 	"gorm.io/gorm"
 )
 
-func Open(conf *database.Config, gormConf *gorm.Config) (db *gorm.DB, err error) {
+func Open(conf *database.Config, opts ...gorm.Option) (db *gorm.DB, err error) {
 	switch conf.Driver {
 	case "mysql":
-		return mysqlOpen(conf, gormConf)
+		return mysqlOpen(conf, opts...)
 	case "postgres":
-		return postgresOpen(conf, gormConf)
+		return postgresOpen(conf, opts...)
 	case "sqlite", "sqlite3":
-		return sqliteOpen(conf, gormConf)
+		return sqliteOpen(conf, opts...)
 	default:
 		return nil, errors.ErrUnknownDatabaseDriver
 	}
 }
 
-func mysqlOpen(conf *database.Config, gormConf *gorm.Config) (db *gorm.DB, err error) {
-	return gorm.Open(mysql.Open(conf.Dsn()), gormConf)
+func mysqlOpen(conf *database.Config, opts ...gorm.Option) (db *gorm.DB, err error) {
+	return gorm.Open(mysql.Open(conf.Dsn()), opts...)
 }
 
-func postgresOpen(conf *database.Config, gormConf *gorm.Config) (db *gorm.DB, err error) {
-	return gorm.Open(postgres.Open(conf.Dsn()), gormConf)
+func postgresOpen(conf *database.Config, opts ...gorm.Option) (db *gorm.DB, err error) {
+	return gorm.Open(postgres.Open(conf.Dsn()), opts...)
 }
 
-func sqliteOpen(conf *database.Config, gormConf *gorm.Config) (db *gorm.DB, err error) {
-	return gorm.Open(sqlite.Open(conf.Dsn()), gormConf)
+func sqliteOpen(conf *database.Config, opts ...gorm.Option) (db *gorm.DB, err error) {
+	return gorm.Open(sqlite.Open(conf.Dsn()), opts...)
 }
